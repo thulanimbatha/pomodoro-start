b28ef0da8ebfb1faae42ccc821e96eeb9677cf01
thulanimbatha <thulanimbatha06@gmail.com>
2022-07-06 21:02:49 +0200

    changed label and button names, added check mark

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from tkinter import *
 from turtle import bgcolor, fd
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -22,8 +23,8 @@ window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)    # add padding to the image
 
 # label
-label = Label(text="Pomodoro")
-label.config(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 45, "italic"))
+label = Label(text="Timer")
+label.config(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 42, "italic"))
 label.grid(column=1, row=0)
 
 # create canvas
@@ -33,13 +34,17 @@ canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-# button 1
-button1 = Button(text="button")
+# Start button 
+button1 = Button(text="Start")
 button1.grid(column=0, row=2)
 
-# button 1
-button2 = Button(text="button")
+# Reset button 
+button2 = Button(text="Reset")
 button2.grid(column=2, row=2)
 
+# check mark
+check_mark = Label(text="✔✅")
+check_mark.config(fg=PINK, bg=YELLOW)
+check_mark.grid(column=1, row=3)
 
 window.mainloop()
