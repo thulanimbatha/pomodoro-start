fea5210a906c3fcd91cdb3bdafaf636c1e7bf92b
thulanimbatha <thulanimbatha06@gmail.com>
2022-07-06 20:44:30 +0200

    added label, buttons to grid layout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from turtle import bgcolor
+from turtle import bgcolor, fd
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -21,11 +21,25 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)    # add padding to the image
 
+# label
+label = Label(text="Pomodoro")
+label.config(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 45, "italic"))
+label.grid(column=1, row=0)
+
 # create canvas
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img) 
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+# button 1
+button1 = Button(text="button")
+button1.grid(column=0, row=2)
+
+# button 1
+button2 = Button(text="button")
+button2.grid(column=2, row=2)
+
 
 window.mainloop()
