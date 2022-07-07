e562435a86db3ac54420152e742848165a24e326
thulanimbatha <thulanimbatha06@gmail.com>
2022-07-08 00:25:28 +0200

    changed count down function

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from tabnanny import check
 from tkinter import *
-from turtle import bgcolor, fd
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,9 +14,17 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    count_down(10 * 60)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+
+    # we need count to be displayed using time format
+    count_min = math.floor(count / 60)  # returns largest whole number smaller than count
+    count_sec = count % 60              # gives back remainder, eg. 100 % 60 = 1 rem. 40
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -36,11 +43,9 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img) 
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
-# call count down method
-count_down(5)
 
-# Start button 
-button1 = Button(text="Start", highlightthickness=0)
+# Start button -> call countdown method
+button1 = Button(text="Start", highlightthickness=0, command=start_timer)
 button1.grid(column=0, row=2)
 
 # Reset button 
