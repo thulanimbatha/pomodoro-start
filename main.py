2d4e2f231fddf6c612701b96a343b18d606661c7
thulanimbatha <thulanimbatha06@gmail.com>
2022-07-08 01:52:24 +0200

    modified timer functionality

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ def count_down(count):
     count_min = math.floor(count / 60)  # returns largest whole number smaller than count
     count_sec = count % 60              # gives back remainder, eg. 100 % 60 = 1 rem. 40
 
+    # dynamic typing - changing the data type of count
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
